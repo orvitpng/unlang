bebba7aa28087c40902298027791f83b2b5d286b
Carter Davis <cdavis4short@gmail.com>
2024-11-19 23:58:23 -0600

    lexer macro

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,7 +1,9 @@
+mod macros;
 mod token;
 
 use std::io::{self, Read};
 
+use macros::iter_matcher;
 use thiserror::Error;
 use token::{Delimiter, Operator, Token};
 
@@ -19,7 +21,9 @@ impl<R: Read> Iterator for Lexer<R> {
     type Item = Result<Token, LexerError>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let c = match self.input.next()? {
+        let input = &mut self.input;
+
+        let c = match input.next()? {
             Ok(b) => b as char,
             Err(err) => return Some(Err(LexerError::IoError(err))),
         };
@@ -28,30 +32,26 @@ impl<R: Read> Iterator for Lexer<R> {
             return self.next();
         }
 
-        Some(Ok(match c {
+        Some(Ok(iter_matcher!(input, c, {
             c if c.is_alphabetic() || c == '_' => {
                 let mut ident = String::from(c);
 
-                while let Some(Ok(next_b)) = self.input.peek() {
+                while let Some(Ok(next_b)) = input.peek() {
                     let next_c = *next_b as char;
                     if !next_c.is_alphanumeric() && next_c != '_' {
                         break;
                     }
 
                     ident.push(next_c);
-                    self.input.next();
+                    input.next();
                 }
 
                 Token::parse_keydentifier(ident)
-            }
-            '_' => Token::Identifier(token::Identifier::Discard),
+            },
 
             '=' => Token::Operator(Operator::Assignment),
             ':' => Token::Operator(Operator::Type),
-            '-' if matches!(self.input.peek(), Some(Ok(b)) if *b as char == '>') => {
-                self.input.next();
-                Token::Operator(Operator::ReturnType)
-            }
+            '-', '>' => Token::Operator(Operator::ReturnType),
 
             '(' => Token::Delimiter(Delimiter::OpenParen),
             ')' => Token::Delimiter(Delimiter::CloseParen),
@@ -59,24 +59,22 @@ impl<R: Read> Iterator for Lexer<R> {
             '}' => Token::Delimiter(Delimiter::CloseBrace),
 
             ',' => Token::Separator,
-            '/' if matches!(self.input.peek(), Some(Ok(b)) if *b as char == '/') =>
-            {
+            '/', '/' => {
                 let mut comment = String::new();
-                self.input.next();
 
                 // hopefully not a problem for error handling
-                while let Some(Ok(b)) = self.input.next() {
-                    if b as char == '\n' {
+                while let Some(Ok(b)) = input.next() {
+                    if b == b'\n' {
                         break;
                     }
                     comment.push(b as char);
                 }
 
                 Token::Comment(comment)
-            }
+            },
 
             _ => return Some(Err(LexerError::UnexpectedChar(c))),
-        }))
+        })))
     }
 }
 
